52c2d0a869868e81acf074842d83cb4550ee7f52
JunDevl <juninhoplay.pedone@gmail.com>
2026-04-15 13:25:39 -0300

    Implemented selected state for table rows

--- client/src/ui/components/Table/Table.tsx
+++ client/src/ui/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import type { HTMLProps } from "react";
+import { useRef, type ChangeEvent, type HTMLProps, type InputEvent } from "react";
 import "./table.css"
 
 type TableProps = {
@@ -7,6 +7,28 @@ type TableProps = {
 } & HTMLProps<HTMLTableElement>
 
 const Table = ({dataArray, title, ...props}: TableProps) => {
+  const selectAll = useRef<HTMLInputElement>(null);
+  const rowSelectors = useRef<HTMLInputElement[]>([]);
+  const body = useRef<HTMLTableSectionElement>(null);
+
+  const selectAllItems = () => {
+    rowSelectors.current.forEach(select => {
+      select!.checked = select!.checked ? false : true;
+
+      const row = select.closest("tr");
+
+      row!.toggleAttribute("selected");
+    })
+  }
+
+  const selectItem = (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+
+    const row = target.closest("tr");
+
+    row!.toggleAttribute("selected");
+  }
+
   return (
     <>
       <h1>{title}</h1>
@@ -16,14 +38,14 @@ const Table = ({dataArray, title, ...props}: TableProps) => {
             <p className="search_icon">
               <svg viewBox="0 0 24 24" fill="transparent" xmlns="http://www.w3.org/2000/svg">
                 <g id="Interface / Search_Magnifying_Glass">
-                  <path id="Vector" d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path id="Vector" d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </g>
               </svg>
             </p>
             <input type="text" className="search" id="search" name="search"/>
             <button className="expand">
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4C3.44772 8 3 7.55228 3 7ZM6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12ZM9 17C9 16.4477 9.44772 16 10 16H14C14.5523 16 15 16.4477 15 17C15 17.5523 14.5523 18 14 18H10C9.44772 18 9 17.5523 9 17Z" fill="currentColor"/>
+                <path fillRule="evenodd" clipRule="evenodd" d="M3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4C3.44772 8 3 7.55228 3 7ZM6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12ZM9 17C9 16.4477 9.44772 16 10 16H14C14.5523 16 15 16.4477 15 17C15 17.5523 14.5523 18 14 18H10C9.44772 18 9 17.5523 9 17Z" fill="currentColor"/>
               </svg>
             </button>
           </div>
@@ -36,23 +58,35 @@ const Table = ({dataArray, title, ...props}: TableProps) => {
             <thead>
               <tr>
                 <th scope="col">
-                  <input type="checkbox" name="all" id="all" />
+                  <input 
+                    type="checkbox"
+                    name="all"   
+                    id="all" 
+                    ref={selectAll}
+                    onChange={selectAllItems}
+                  />
                 </th>
                 {Object.keys(dataArray[0]).map(item => 
-                  <th scope="col">
+                  <th scope="col" key={item}>
                     {item}
                   </th>
                 )}
               </tr>
             </thead>
-            <tbody>
+            <tbody ref={body}>
               {dataArray.map((item, index) =>
-                <tr>
+                <tr id={`row${index+1}`} key={`row${index+1}`}>
                   <th scope="row">
-                    <input type="checkbox" name="select" id={`row${index+1}`} />
+                    <input 
+                      ref={element => {rowSelectors.current[index] = element as HTMLInputElement}}
+                      type="checkbox" 
+                      name="select" 
+                      className="select"
+                      onChange={e => selectItem(e)}
+                    />
                   </th>
-                  {Object.values(item).map(value =>
-                    <td>{value instanceof Date ? value.toLocaleString() : value}</td>
+                  {Object.values(item).map((value, index) =>
+                    <td key={index}>{value instanceof Date ? value.toLocaleString() : value}</td>
                   )}
                 </tr>
               )}
